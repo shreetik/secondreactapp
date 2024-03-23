b1e95fc5dc707044f1752839efa995739e242c57
duttasourav1999 <duttasourav1999@gmail.com>
2024-03-24 02:01:23 +0530

    changed name

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ let root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
 
-    <App name="Sourav" desc="Software Developer" age="24"/>
+    <App name="Tanmoy" desc="Software Developer" age="24"/>
     <App name="Shreetik" desc="Senior Software Developer" age="24"/>
     <App name="Bishal" desc="Graphic Designer" age="24"/>
 
